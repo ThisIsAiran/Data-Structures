13cee54843792d8c0f432451b43c71fe1d67b9c6
guptaaman70659 <guptaaman70659@gmail.com>
2019-01-27 00:01:10 +0530

    second insert a no. commit

--- insertion.cpp
+++ insertion.cpp
@@ -2,20 +2,27 @@
 using namespace std;
 int main()
 {
-	int a[11] = {1,2,3,4,6,7,8,9,10,11},y;
+	int a[11] = {1,2,3,4,6,7,8,9,10,11},y,z,temp;
+	cout << "Enter the number you want to insert\n";
 	cin >> y;
-	for (int i = 9; i>=0 ; i=i-1)
+	cout << "Enter the element after which you want to insert the number\n";
+	cin >> z;
+	a[10] = y;
+	for (int i = 9;i>=0;i=i-1)
 	{
-		if(a[i]>y)
+		if(a[i]!=z)
+		{
+		temp = a[i+1];
 		a[i+1] = a[i];
+		a[i] = temp;
+		}
 		else
 		{
-			a[i+1] = y;
 			break;
 		}
 	}
 	for (int i = 0; i < 11; ++i)
 	{
-		cout << a[i];
+		cout << a[i] << " ";
 	}
 }
